36787ac247bdaf4c51c95d74648b4b9868608681
salma <sm.seemo25@gmail.com>
2022-10-26 10:58:20 +0300

    Initial Commit

---
+++ star/starApp.swift
@@ -0,0 +1,17 @@
+//
+//  starApp.swift
+//  star
+//
+//  Created by Salma on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct starApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
